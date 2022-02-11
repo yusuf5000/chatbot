02bd82253b61ce197cc9b90f2d31dec7725b9580
thegoldencheesecake <78363661+thegoldencheesecake@users.noreply.github.com>
2022-02-11 21:15:42 +0400

    Add files via upload

--- main.py
+++ main.py
@@ -13,7 +13,7 @@
 
 from chatterbot import ChatBot
 
-name = 'bot'
+name = "bot"
 bot = ChatBot(
     name,
     storage_adapter="chatterbot.storage.SQLStorageAdapter",
@@ -24,15 +24,16 @@ bot = ChatBot(
         {
             "import-path": "chatterbot.logic.BestMatch",
             "default_response": "I am sorry, I don't understand you.",
-            "maximum_similarity_threshold": 0.50
-        }
+            "maximum_similarity_threshold": 0.50,
+        },
     ],
-    database_uri = "sqlite:///database.sqlite3"
+    database_uri="sqlite:///database.sqlite3",
 )
 
 """Making the trainers and training the bot using ready made data"""
 
-from chatterbot.trainers import ListTrainer, ChatterBotCorpusTrainer, UbuntuCorpusTrainer
+from chatterbot.trainers import (ChatterBotCorpusTrainer, ListTrainer,
+                                 UbuntuCorpusTrainer)
 
 trainer = ListTrainer(bot)
 trainerCorpus = ChatterBotCorpusTrainer(bot)
@@ -42,30 +43,31 @@ trainerCorpus.train("chatterbot.corpus.english")
 
 """The Main Program"""
 
-from colorama import init, Fore, Style
-import art
 import os
+
+import art
+from colorama import Fore, Style, init
 
 init()
 
-print(art.text2art("Bot", "starwars")) 
+print(art.text2art("Bot", "starwars"))
 
 uname = input("What is your name? ")
 bname = input("What would you like to name the bot? ")
 print("")
 
 while True:
-    if os.system('color') == 1:
-      query = input(f"{Fore.WHITE}{uname}>{Style.RESET_ALL} ")
+    if os.system("color") == 1:
+        query = input(f"{Fore.WHITE}{uname}>{Style.RESET_ALL} ")
     else:
-      query = input(f"{uname}> ")
+        query = input(f"{uname}> ")
 
     if query == "exit":
         print("Exiting...")
         break
 
     answer = bot.get_response(query)
-    if os.system('color') == 1:
-      print(f"{Fore.YELLOW}{bname}>{Style.RESET_ALL}", answer)
+    if os.system("color") == 1:
+        print(f"{Fore.YELLOW}{bname}>{Style.RESET_ALL}", answer)
     else:
-      print(f"{bname}>", answer)
+        print(f"{bname}>", answer)
